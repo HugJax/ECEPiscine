0e6e589499fe0735be61699a673aad7ba338bc66
rf176131 <47567983+rf176131@users.noreply.github.com>
2020-04-18 14:50:18 +0200

    Add files via upload

--- chat.php
+++ chat.php
@@ -28,9 +28,8 @@
         if(isset($_POST['prixPropose'])||(isset($_POST['commentaire'])))           //le prix est proposé par l'acheteur
         {  
             
-            //Ces variables doivent être changées pour intégrer la page 
-            //$Prix = 12;
-            $IDproduit = 1;
+            //CES VARIABLES DOIVENT ÊTRE MODIFIEES DANS LA VERSION FINALE CAR IL FAUT RECUPERER L'IDENTIFIANT DU PRODUIT PUIS CELUI DE SON VENDEUR
+            $IDproduit = 2;
             $IDvendeur = 3;
             
             $propositions = "SELECT COUNT(*)  FROM `negociation` WHERE ((IDutilisateur='".$IDacheteur."') AND (IDproduit='".$IDproduit."'))";      //On compte le nombre de demandes effectuées par un acheteur sur un produit donné
@@ -40,47 +39,130 @@
             $reponse      = mysqli_fetch_array($exec_requete);
             $count = $reponse['COUNT(*)'];
             
-            if($count < 5 )       //Si on est à moins de 5 propositions
+            
+            //On récupère le booléen spécifiant si la proposition de l'acheteur connecté est satisfaite
+            $etatVente = "SELECT Vendu FROM produit WHERE (IDproduit='".$IDproduit."') "; 
+            
+            // $db_handle->query($acceptation);           //On effectue la requête précédente 
+            
+            $exec_requete = mysqli_query($db_handle,$etatVente);
+            $reponse      = mysqli_fetch_array($exec_requete);
+            $achat = $reponse['Vendu'];
+            
+            ///////////////////////////////////////
+            ///////////////////////////////////////
+            /////////////////////////////////////////
+            /////////////////////////////////////////////
+            $verification1 ="SELECT adresse.IDadresse 
+                    FROM adresse 
+                    INNER JOIN utilisateur ON adresse.IDadresse = utilisateur.IDadresse 
+                    WHERE (utilisateur.IDutilisateur = '".$IDacheteur."')";
+                
+                
+            $exec_requete = mysqli_query($db_handle,$verification1);
+            $reponse      = mysqli_fetch_array($exec_requete);
+            $coordonneesA = $reponse['IDadresse'];
+                
+            $verification2 ="SELECT paiement.IDpaiement 
+                    FROM paiement 
+                    INNER JOIN utilisateur ON paiement.IDpaiement = utilisateur.IDpaiement 
+                    WHERE (utilisateur.IDutilisateur = '".$IDacheteur."')";
+
+            $exec_requete = mysqli_query($db_handle,$verification2);
+            $reponse      = mysqli_fetch_array($exec_requete);
+            $coordonneesP = $reponse['IDpaiement'];
+                
+
+            $datetime = date("Y-m-d H:i:s");          //Récupération de la date du jour
+
+            $prix = "SELECT Prix FROM negociation WHERE ((IDproduit='".$IDproduit."') AND Acceptation=1 AND EtatVente=1) "; 
+            
+            $exec_requete = mysqli_query($db_handle,$prix);
+            $reponse      = mysqli_fetch_array($exec_requete);
+            $prixAchat = $reponse['Prix'];            
+            
+            
+            
+            ////////////////////////////////
+                        ///////////////////////////////
+            /////////////////////////////////////
+            //////////////////////////////////////
+            /////////////////////////////////////
+                        
+            if($achat == 0)                 //Si le produit est toujours disponible à la vente 
             {
+                if($count < 5 )       //Si on est à moins de 5 propositions
+                {
            
-                //On insère la proposition d'achat dans la table négociation
-                $requete1 = "INSERT INTO `negociation`(`IDproduit`, `IDutilisateur`, `Commentaire`, `Prix`, `EtatVente`, `Acceptation`) VALUES ((SELECT IDproduit FROM produit WHERE IDproduit='".$IDproduit."'), (SELECT IDutilisateur FROM utilisateur WHERE IDutilisateur='".$IDacheteur."'), '".$commentaire."', '".$prixPropose."' ,0,0)";
+                    //On insère la proposition d'achat dans la table négociation
+                    $requete1 = "INSERT INTO `negociation`(`IDproduit`, `IDutilisateur`, `Commentaire`, `Prix`, `EtatVente`, `Acceptation`) VALUES ((SELECT IDproduit FROM produit WHERE IDproduit='".$IDproduit."'), (SELECT IDutilisateur FROM utilisateur WHERE IDutilisateur='".$IDacheteur."'), '".$commentaire."', '".$prixPropose."' ,0,0)";
            
-                $db_handle->query($requete1);           //On effectue la requête d'insertion précédente
+                    $db_handle->query($requete1);           //On effectue la requête d'insertion précédente
       
-                echo "Je vous propose". $prixPropose. "euros";
+                    echo "Je vous propose". $prixPropose. "euros";
                         
             
-                //On récupère le booléen spécifiant si la proposition de l'acheteur connecté est satisfaite
-                $acceptation = "SELECT Acceptation FROM negociation WHERE (IDutilisateur = '".$IDacheteur."' AND IDproduit='".$IDproduit."') "; 
-            
-               // $db_handle->query($acceptation);           //On effectue la requête précédente 
-            
-                $exec_requete = mysqli_query($db_handle,$acceptation);
-                $reponse      = mysqli_fetch_array($exec_requete);
-                $count = $reponse['Acceptation'];
-                
-                if($acceptation == 1)               //Si le vendeur accepte l'offre
-                {
-                    echo "Bravo, votre offre a été acceptée par l'acheteur";
-                    //Suppression de votre ancienne proposition de la table
-                    $requete3 = "DELETE FROM `negociation` WHERE IDutilisateur='".$IDacheteur."' ";       
-                    $db_handle->query($requete3);
+                    //On récupère le booléen spécifiant si la proposition de l'acheteur connecté est satisfaite
+                    $acceptation = "SELECT Acceptation FROM negociation WHERE (IDutilisateur = '".$IDacheteur."' AND IDproduit='".$IDproduit."') "; 
+                        
+                    $exec_requete = mysqli_query($db_handle,$acceptation);
+                    $reponse      = mysqli_fetch_array($exec_requete);
+                    $count = $reponse['Acceptation'];
+                                    
+                    if($count == 1)               //Si le vendeur accepte l'offre
+                    {
+                        echo "Bravo, votre offre a été acceptée par l'acheteur";
+                        //Suppression de votre ancienne proposition de la table
+                        $requete3 = "DELETE FROM `negociation` WHERE IDutilisateur='".$IDacheteur."' ";       
+                        $db_handle->query($requete3);
+                        
+                        
+                        if($verification1 == 1)             //Si l'adresse de l'acheteur est bien enregistrée
+                        {
+                            if($verification2 == 1)        //Si les coordonnées  bancaires de l'acheteur sont bien enregistrées
+                            {
+                                //Insertion de la transaction dans la table transaction 
+                                $confirmationAchat = "INSERT INTO `transaction`(`IDtransaction`, `IDproduit`, `IDutilisateur`, `Prix`, `Date`) VALUES (IDtransaction,".$IDproduit.",'".$IDacheteur."','".$prixAchat."', '".$datetime."')";  
+                                $db_handle->query($confirmationAchat);
+                        
+                                //On notifie que le produit a été vendu
+                                $modificationStatutVenteProduit = " UPDATE produit SET Vendu = 1 WHERE IDproduit='".$IDproduit."' ";      
+                                $db_handle->query($modificationStatutVenteProduit);
+                                
+                                echo "Bravo, votre nouvelle acquisition a été prise en compte";
+                        
+                            }
+                            else
+                            {
+                                echo "Veuillez ajouter vos coordonnées bancaires";
+                            }
+                        
+                        }   
+                        else
+                        {
+                            echo "Veuillez ajouter une adresse de livraison";
+                        }
 
-                }
-                if($acceptation == 0)                     //Si le vendeur refuse l'offre
-                {
-                    echo "Votre offre n'a pas été acceptée, Le vendeur va vous faire une contre-offre, il vous reste " .$count. "propositions";
-                    //Suppression de votre ancienne proposition de la table 
-                    $requete4 = "DELETE FROM `negociation`(`Commentaire`) VALUES ('".$prixPropose."')";  
-                    $db_handle->query($requete4);
+                    }
+                    if($count == 0)                     //Si le vendeur refuse l'offre
+                    {
+                        echo "Votre offre n'a pas été acceptée, Le vendeur va vous faire une contre-offre, il vous reste " .$count. "propositions";
+                        //Suppression de votre ancienne proposition de la table 
+                        $requete4 = "DELETE FROM `negociation`(`Commentaire`) VALUES ('".$prixPropose."')";  
+                        $db_handle->query($requete4);
          
+                    }
                 }
+                else
+                {
+                    echo "Vous avez déjà fait cinq offres pour ce produit, désolé mais vous ne pouvez plus acheter ce produit";
+                    //Blindage
+                }
+            
             }
             else
             {
-                echo "Vous avez déjà fait cinq offres pour ce produit, désolé mais vous ne pouvez plus acheter ce produit";
-                //Blindage
+                echo "Désolé, ce produit a déjà été acheté par un autre utilisateur";
             }
             
         }
